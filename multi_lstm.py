74bcde849b10c8eda7ed6fd0a4e7396bf812b809
Osama Muhammed <42488643+osama1998H@users.noreply.github.com>
2023-04-21 18:37:13 +0300

    Update multi_lstm.py

--- multi_lstm.py
+++ multi_lstm.py
@@ -8,6 +8,8 @@ import MetaTrader5 as mt5
 if not mt5.initialize():
     print("initialize() failed, error code =", mt5.last_error())
     quit()
+    
+symbols = mt5.symbols_get(group="EUR,USD")
 
 # Download historical data for all symbols
 closing_prices = []
